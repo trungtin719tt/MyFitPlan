466f83c0505e675524c6400c56f58b32e0acb165
Tín Nguyễn Trung <trungtin719.tt@gmail.com>
2019-03-03 12:16:37 +0700

    commit

--- MyFitPlan/app/src/main/java/mobile/myfitplan/myfitplan/ForgotPassword2.java
+++ MyFitPlan/app/src/main/java/mobile/myfitplan/myfitplan/ForgotPassword2.java
@@ -2,15 +2,12 @@ package mobile.myfitplan.myfitplan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotPassword2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal);
-        Intent  intent = new Intent(this, Personal.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_forgot_password2);
     }
 }
---
+++ MyFitPlan/app/src/main/java/mobile/myfitplan/myfitplan/PersonalMenu.java
@@ -0,0 +1,26 @@
+package mobile.myfitplan.myfitplan;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PersonalMenu.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link PersonalMenu#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PersonalMenu extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_personal_menu, container, false);
+        return rootView;
+    }
+}
