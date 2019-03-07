c06117b54dd548ce36cc5a4d6109be1bebee968a
Tín Nguyễn Trung <trungtin719.tt@gmail.com>
2019-03-07 21:06:56 +0700

    commit before merge

--- MyFitPlan/app/src/main/java/mobile/myfitplan/myfitplan/MainActivity.java
+++ MyFitPlan/app/src/main/java/mobile/myfitplan/myfitplan/MainActivity.java
@@ -3,6 +3,7 @@ package mobile.myfitplan.myfitplan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
         Intent  intent = new Intent(this, Personal.class);
+        startActivity(intent);
+    }
+    //click để hiện popup
+    public void clickToPopUpNewFolder(View view) {
+        Intent intent = new Intent(this, NewFolder.class);
         startActivity(intent);
     }
 }
--- MyFitPlan/app/src/main/java/mobile/myfitplan/myfitplan/PersonalMenu.java
+++ MyFitPlan/app/src/main/java/mobile/myfitplan/myfitplan/PersonalMenu.java
@@ -7,20 +7,110 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+
+import org.w3c.dom.Text;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PersonalMenu.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PersonalMenu#newInstance} factory method to
- * create an instance of this fragment.
- */
+///**
+// * A simple {@link Fragment} subclass.
+// * Activities that contain this fragment must implement the
+// * {@link PersonalMenu.OnFragmentInteractionListener} interface
+// * to handle interaction events.
+// * Use the {@link PersonalMenu#newInstance} factory method to
+// * create an instance of this fragment.
+// */
 public class PersonalMenu extends Fragment {
+    private LinearLayout mainLayout, foodDisplay, foodDisplay1;
+    private TextView txtPlus, txtPlus1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_personal_menu, container, false);
+        LinearLayout linearLayout = (LinearLayout) rootView.findViewById(R.id.popUp);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), NewFolder.class));
+            }
+        });
+        LinearLayout linearLayoutMinimize = (LinearLayout) rootView.findViewById(R.id.minimize_layout);
+        linearLayoutMinimize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (foodDisplay.getVisibility() == View.VISIBLE) {
+                    setLayoutInvisible(foodDisplay, txtPlus);
+                } else {
+                    setLayoutVisible(foodDisplay, txtPlus);
+                }
+            }
+        });
+        LinearLayout linearLayoutMinimize1 = (LinearLayout) rootView.findViewById(R.id.minimize_layout1);
+        linearLayoutMinimize1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (foodDisplay1.getVisibility() == View.VISIBLE) {
+                    setLayoutInvisible(foodDisplay1, txtPlus1);
+                } else {
+                    setLayoutVisible(foodDisplay1, txtPlus1);
+                }
+            }
+        });
+        foodDisplay = (LinearLayout)rootView.findViewById(R.id.foodDisplay);
+        txtPlus = (TextView)rootView.findViewById(R.id.txtPlus);
+        foodDisplay1 = (LinearLayout) rootView.findViewById(R.id.foodDisplay1);
+        txtPlus1 = (TextView) rootView.findViewById(R.id.txtPlus1);
         return rootView;
+    }
+//    public void clickToMinimizeMaxmimize(LinearLayout LL) {
+//        if (LL.getVisibility() == View.VISIBLE) {
+//            setLayoutInvisible();
+//        } else {
+//            setLayoutVisible();
+//        }
+//    }
+
+    public void clickToPopUp(View view) {
+        Intent intent = new Intent(getActivity(), NewFolder.class);
+        startActivity(intent);
+    }
+
+    public void clickToMinimizeMaxmimize1(View view) {
+        if (foodDisplay1.getVisibility() == View.VISIBLE) {
+            setLayoutInvisible1();
+        } else {
+            setLayoutVisible1();
+        }
+
+
+    }
+    public void setLayoutInvisible(LinearLayout LL, TextView TV) {
+        if (LL.getVisibility() == View.VISIBLE) {
+            LL.setVisibility(View.GONE);
+            TV.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void setLayoutVisible(LinearLayout LL, TextView TV) {
+        if (LL.getVisibility() == View.GONE) {
+            LL.setVisibility(View.VISIBLE);
+            TV.setVisibility(View.GONE);
+        }
+    }
+
+    public void setLayoutVisible1() {
+        if (foodDisplay1.getVisibility() == View.GONE) {
+            foodDisplay1.setVisibility(View.VISIBLE);
+            txtPlus1.setVisibility(View.GONE);
+        }
+    }
+
+    public void setLayoutInvisible1() {
+        if (foodDisplay1.getVisibility() == View.VISIBLE) {
+            foodDisplay1.setVisibility(View.GONE);
+            txtPlus1.setVisibility(View.VISIBLE);
+        }
     }
 }
